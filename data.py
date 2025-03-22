f7950209bff7d83688c9a259f5d8cc90c6379907
Luis Arturo Medina Villalobos <luisarturomedina84@gmail.com>
2025-03-22 18:43:59 -0300

    Finally!

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-43827dc2-2bb3-4117-b00c-63efd3142284.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-d2f0f3ee-7e89-431d-a9e8-82ea2fd44ec5.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
